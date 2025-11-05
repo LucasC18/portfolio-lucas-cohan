5f191a1d13220ca61eb0f726e12213704cf48795
LucasC18 <lucascohanedu@gmail.com>
2025-11-05 15:29:48 -0300

    Fix: ajustar rutas y usar HashRouter para despliegue en GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
+  base: "/portfolio-lucas-cohan/",
   plugins: [react()],
-  
-  base: '/portfolio-lucas-cohan/', 
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
