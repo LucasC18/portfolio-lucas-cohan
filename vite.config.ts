4f3f8c571f873b4f8ffae9b1887dc1a9c988097a
LucasC18 <lucascohanedu@gmail.com>
2025-11-05 14:39:59 -0300

    build: deploy manual de dist para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
 
 export default defineConfig({
+  base: "/portafolio-lucas-cohan/",
   plugins: [react()],
   resolve: {
     alias: {
